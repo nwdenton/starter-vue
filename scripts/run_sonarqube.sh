24847070f3fea00f150c528392d069e95c3fa813
Nina Charness <ncharness@pivotal.io>
2019-09-30 16:09:04 -0400

    remove breaking directory change

--- scripts/run_sonarqube.sh
+++ scripts/run_sonarqube.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-export SCRIPT_DIR="$(dirname "$(stat -f "$0")")"
-pushd $SCRIPT_DIR/..
-
 ./gradlew sonarqube \
   -Dsonar.projectKey=Maestro \
   -Dsonar.host.url=https://sonarqube.dev.s2p.cloud \
   -Dsonar.login=$SONARQUBE_LOGIN
 
-popd
